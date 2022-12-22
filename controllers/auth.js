821b7c4cf5d3c669d044017f86170c491420a024
Eder Ramirez <eder.ramirez87@gmail.com>
2022-12-21 18:16:04 -0600

    generate JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require("express");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const { generateJWT } = require("../helpers/jwt");
 
 const createUser = async (
   req,
@@ -23,15 +24,21 @@ const createUser = async (
     // Encrypt password
     const salt = bcrypt.genSaltSync();
     user.password = bcrypt.hashSync(password, salt);
+    console.log("🚀 ~ file: auth.js:27 ~ user", user);
 
     await user.save();
+
+    // Generate JWT
+    const token = await generateJWT(user.id, user.name);
 
     res.status(201).json({
       ok: true,
       uid: user.id,
       name: user.name,
+      token,
     });
   } catch (error) {
+    console.log("🚀 ~ file: auth.js:40 ~ error", error);
     res.status(500).json({
       ok: false,
       msg: "Error saving the user, please reach out the db admin",
@@ -63,11 +70,13 @@ const loginUser = async (req, res = response) => {
     }
 
     // Ready to generate JWT
+    const token = await generateJWT(user.id, user.name);
 
     res.json({
       ok: true,
       uid: user.id,
       name: user.name,
+      token,
     });
   } catch (error) {
     res.status(500).json({
